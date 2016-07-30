c90c96428a8fc0b6840d99c0eeab7b02f08c5627
Masahiro Watanabe <m@nsocean.org>
2016-07-30 12:03:40 +0900

    Add more example usages to Demo

--- Demo/Demo/DemoViewController.swift
+++ Demo/Demo/DemoViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class DemoViewController: UIViewController {
-
+    private enum ActionTitle: String {
+        case Alert
+        case ActionSheet
+        case Login
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let items = [
             UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil),
-            UIBarButtonItem(title: "Login", style: .Plain, target: self, action: #selector(DemoViewController.didTapButton(_:))),
-            UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+            UIBarButtonItem(title: ActionTitle.Alert.rawValue, style: .Plain, target: self, action: #selector(DemoViewController.didTapButton(_:))),
+            UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: ActionTitle.ActionSheet.rawValue, style: .Plain, target: self, action: #selector(DemoViewController.didTapButton(_:))),
+            UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: ActionTitle.Login.rawValue, style: .Plain, target: self, action: #selector(DemoViewController.didTapButton(_:))),
+            UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil),
         ]
         
         self.navigationController?.setToolbarHidden(false, animated: false)
@@ -25,21 +34,49 @@ class DemoViewController: UIViewController {
     
     func didTapButton(sender: UIBarButtonItem){
         
-        ALRT.create(.Alert, title: "Login", message: "Please enter your credentials")
-            .addTextField { textField in
-                textField.placeholder = "Username"
-            }
-            .addTextField { textField in
-                textField.placeholder = "Password"
-                textField.secureTextEntry = true
-            }
-            .addCancel()
-            .addOK() { alert, textFields in
-                textFields?
-                    .flatMap { (placeholder: $0.placeholder ?? "No Placeholder", text: $0.text ?? "No Text") }
-                    .forEach { print("\($0.placeholder) => \($0.text)") }
-            }
-            .show()
+        guard let title = sender.title else {
+            return
+        }
+        
+        if title == ActionTitle.Alert.rawValue {
+            
+            ALRT.create(.Alert, title: "Error", message: "No item found")
+                .addOK()
+                .show()
+            
+        } else if title == ActionTitle.ActionSheet.rawValue {
+            
+            ALRT.create(.ActionSheet, title: "Destination", message: "Please select your destination")
+                .addPopoverPresentation {
+                    // implement popover.barButtonItem or popover.sourceView for iPad
+                    popover in
+                    popover?.barButtonItem = sender
+                }
+                .addAction("New York") { action, textFields in print("New York has been selected") }
+                .addAction("Paris")
+                .addAction("London")
+                .addDestructive("Not interested")
+                .show()
+            
+        } else if title == ActionTitle.Login.rawValue {
+            
+            ALRT.create(.Alert, title: "Login", message: "Please enter your credentials")
+                .addTextField { textField in
+                    textField.placeholder = "Username"
+                }
+                .addTextField { textField in
+                    textField.placeholder = "Password"
+                    textField.secureTextEntry = true
+                }
+                .addCancel()
+                .addOK() { alert, textFields in
+                    textFields?
+                        .flatMap { (placeholder: $0.placeholder ?? "No Placeholder", text: $0.text ?? "No Text") }
+                        .forEach { print("\($0.placeholder) => \($0.text)") }
+                }
+                .show() { print("Alert has been shown") }
+            
+        }
     }
     
 }
