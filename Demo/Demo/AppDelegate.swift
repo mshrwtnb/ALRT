8d77d58656e317bc1df78e7678ec85d7545aa1f7
Masahiro Watanabe <iammshr@gmail.com>
2017-09-16 16:29:41 +0900

    Renew DemoViewController

--- Demo/Demo/AppDelegate.swift
+++ Demo/Demo/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Demo
-//
-//  Created by Masahiro Watanabe on 7/30/16.
-//  Copyright © 2016 Masahiro Watanabe. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -14,12 +6,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = UINavigationController(rootViewController: DemoViewController())
-        self.window?.backgroundColor = UIColor.white
-        self.window?.makeKeyAndVisible()
         return true
     }
-
 }
-
--- Demo/Demo/DemoViewController.swift
+++ Demo/Demo/DemoViewController.swift
@@ -1,83 +1,26 @@
 import UIKit
 
 class DemoViewController: UIViewController {
-    fileprivate enum ActionTitle: String {
-        case alert
-        case actionSheet
-        case login
-    }
+    
+    @IBOutlet weak var alertButton: UIButton!
+    @IBOutlet weak var actionSheetButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let items = [
-            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
-            UIBarButtonItem(title: ActionTitle.alert.rawValue, style: .plain, target: self, action: #selector(DemoViewController.didTapButton(_:))),
-            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
-            UIBarButtonItem(title: ActionTitle.actionSheet.rawValue, style: .plain, target: self, action: #selector(DemoViewController.didTapButton(_:))),
-            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
-            UIBarButtonItem(title: ActionTitle.login.rawValue, style: .plain, target: self, action: #selector(DemoViewController.didTapButton(_:))),
-            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
-        ]
-        
-        self.navigationController?.setToolbarHidden(false, animated: false)
-        self.setToolbarItems(items, animated: false)
     }
     
-    func didTapButton(_ sender: UIBarButtonItem){
-        
-        guard
-            let title = sender.title,
-            let actionTitle = ActionTitle(rawValue: title)
-        else {
-            return
-        }
-        
-        switch actionTitle {
-        case .alert:
-            ALRT.create(.alert, title: "Breaking News", message: "MagSafe is gone")
-                .addOK()
-                .addAction("NO WAY", preferred: true) // preferredAction is available iOS 9.0 or later
-                .show()
-        
-        case .actionSheet:
-            ALRT.create(.actionSheet, title: "Destination", message: "Please select your destination")
-                .configurePopoverPresentation {
-                    // set popover.barButtonItem or popover.sourceView for iPad
-                    popover in
-                    popover?.barButtonItem = sender
-                }
-                .addAction("New York") { action, textFields in print("New York has been selected") }
-                .addAction("Paris")
-                .addAction("London")
-                .addDestructive("Not interested")
-                .show()
-            
-        case .login:
-            ALRT.create(.alert, title: "Login", message: "Please enter your credentials")
-                .addTextField { textField in
-                    textField.placeholder = "Username"
-                    textField.accessibilityIdentifier = "Username"
-                }
-                .addTextField { textField in
-                    textField.placeholder = "Password"
-                    textField.accessibilityIdentifier = "Password"
-                    textField.isSecureTextEntry = true
-                }
-                .addCancel()
-                .addOK()
-                .show { result in
-                    switch result {
-                    case .success:
-                        print("The alert is displayed.")
-                        
-                    case .failure(let error):
-                        print("The alert is not displayed. Error => \(error)")
-                    }
-                }
-        }
-
+    @IBAction func didTapAlertButton(_ sender: UIButton) {
+        ALRT.create(.alert, title: "Show me some alert")
+            .addOK()
+            .addCancel()
+            .show()
     }
     
+    @IBAction func didTapActionSheetButton(_ sender: UIButton) {
+        ALRT.create(.actionSheet, title: "ALRT", message: "Show me some action sheet")
+            .addAction("Option A") { _ in print("Option A has been tapped!") }
+            .addAction("Option B") { _ in print("Option B has been tapped!") }
+            .addDestructive("Destructive Option")
+            .show()
+    }
 }
-
